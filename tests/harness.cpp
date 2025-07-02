d4d755d3fac16945f60579961cbbe02e9dc60acb
Nathan5563 <ndawit611@gmail.com>
2025-07-02 01:02:26 -0400

    feat: Get obj file from cmdline args

--- tests/harness.cpp
+++ tests/harness.cpp
@@ -3,13 +3,21 @@
 
 #include "../include/mesh.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-    Mesh mesh;
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <path_to_obj_file>" << std::endl;
+        return 1;
+    }
 
-    std::string path = /* insert .obj file path here */;
+    // WARNING: no error checking here
+    std::string path = argv[1];
+
+    Mesh mesh;
 
     mesh.fromObj(path);
     
     mesh.toObj();
+
+    return 0;
 }
